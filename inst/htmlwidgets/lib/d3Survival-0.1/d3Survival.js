7cb1f4d97c00fd16db5b42e8752b2a64a90fbef3
Coin <tcoi@subaru.com>
2019-05-20 16:47:59 -0400

    Added Brush Zooming Changed Style

--- inst/htmlwidgets/lib/d3Survival-0.1/d3Survival.js
+++ inst/htmlwidgets/lib/d3Survival-0.1/d3Survival.js
@@ -1,12 +1,11 @@
 d3.survival = function(message) {
   
-  tester = message;
-  
-  var margin = {top:10,left:50,bottom:50,right:40},
+  var margin = {top:10,left:75,bottom:50,right:40},
     width = 960 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom,
     xlim = valPropLen(message.options,'xlim'),
     ylim = valPropLen(message.options,'ylim'),
+    lastExtent = [[0,0],[0,0]],
     svg;
 
 
@@ -46,36 +45,38 @@ d3.survival = function(message) {
 
     var color = d3.scaleOrdinal(d3.schemeCategory10)
       .domain(d3.map(nest, function(d) { return d.key}))
+      
+      
+    var domain = {
+        x: xlim || d3.extent(message.data, function(d) { return d['time']}),
+        y: ylim|| [d3.min(message.data, function(d) { return d['conf.low']}), d3.max(message.data, function(d) { return d["conf.high"]})]
+    }
 
     var x = d3.scaleLinear()
-      .domain(xlim || d3.extent(message.data, function(d) { return d['time']}))
+      .domain(domain.x)
       .range([0,width])
       .nice();
 
     var y = d3.scaleLinear()
-      .domain(ylim|| [d3.min(message.data, function(d) { return d['conf.low']}), d3.max(message.data, function(d) { return d["conf.high"]})])
+      .domain(domain.y)
       .range([height,0])
       .nice();
 
-    var xAxis = d3.axisBottom(x);
-
-    var yAxis = d3.axisLeft(y)
-      .tickFormat(d3.format('.2p'))
-    
-    svg.append('g')
-      .classed('axis', true)
-      .attr('transform','translate(0,' + height + ')')
-      .call(xAxis);
-
-    svg.append('g')
-      .classed('axis', true)
-      .call(yAxis)
+    var yAxis = d3.axisLeft(y).tickFormat(d3.format('.2%')),
+        yGrid = d3.axisLeft(y).tickSizeInner(-width),
+        xAxis = d3.axisBottom(x),
+        xGrid = d3.axisBottom(x).tickSizeInner(-height);
       
-    svg.append('clipPath')
-      .attr('id', 'limit-clip')
-      .append('rect')
-      .attr('height', height)
-      .attr('width', width)
+    var brush = d3.brush()
+      .extent([[0,0],[width,height]])
+      .on("end", brushed)
+    
+      
+    svg.append("defs").append("clipPath")
+        .attr("id", "limit-clip")
+        .append("rect")
+        .attr("width", width)
+        .attr("height", height);
     
     var line = d3.line()
       .x(function(d) { return x(d.time)})
@@ -85,25 +86,89 @@ d3.survival = function(message) {
       .x(function(d) { return x(d.time)})
       .y0(function(d) { return y(d['conf.low'])})
       .y1(function(d) { return y(d['conf.high'])});
+      
+      
+    svg.append('g')
+        .classed('grid y',true)
+        .call(yGrid)
+
+    svg.append('g')
+        .classed('grid x',true)
+        .attr('transform','translate(0,' + height + ')')
+        .call(xGrid)
+
+    var strata = svg.append('g')
+        .attr('clip-path','url(#limit-clip)')
+        .selectAll('.strata')
+        .data(nest)
+        .enter()
+        .append('g')
 
 
-    svg.selectAll('.estimate')
-      .data(nest)
-      .enter()
-      .append('path')
-      .attr('clip-path','url(#limit-clip)')
-      .classed('estimate',true)
-      .attr('d', function(d) { return line(d.values)})
-      .style('stroke', function(d) { return color(d.key)});
-
-    svg.selectAll('.confidence')
-      .data(nest)
-      .enter()
-      .append('path')
-      .attr('clip-path','url(#limit-clip)')
+    var conf = strata.append('path')
       .classed('confidence',true)
       .attr('d', function(d) { return area(d.values)})
       .style('fill', function(d) { return color(d.key)});
+      
+      
+    var estimate = strata.append('path')
+      .classed('estimate',true)
+      .attr('d', function(d) { return line(d.values)})
+      .style('stroke', function(d) { return color(d.key)});
+      
+      
+    svg.append('g')
+      .classed('axis x', true)
+      .attr('transform','translate(0,' + height + ')')
+      .call(xAxis);
+
+    svg.append('g')
+      .classed('axis y', true)
+      .call(yAxis)
+
+    svg.append("g")
+      .attr("class", "brush")
+      .call(brush)
+      
+      
+    function brushed() {
+        
+        function arrangeSelection(sel) {
+            return [
+                [d3.min([sel[0][0],sel[1][0]]),d3.max([sel[0][1],sel[1][1]])],
+                [d3.max([sel[0][0],sel[1][0]]),d3.min([sel[0][1],sel[1][1]])]
+            ]
+        }
+        
+        function flatten(x) {
+                  return [x[0][0],x[0][1],x[1][0],x[1][1]];
+        }
+        
+        function zoomChart(xDomain, yDomain) {
+            x.domain(xDomain).nice()
+            y.domain(yDomain).nice() 
+            conf.attr('d', function(d) { return area(d.values)})
+            estimate.attr('d', function(d) { return line(d.values)})
+            svg.select('.axis.y').call(yAxis)
+            svg.select('.grid.y').call(yGrid)
+            svg.select('.axis.x').call(xAxis)
+            svg.select('.grid.x').call(xGrid)
+        }
+
+        if (d3.event.sourceEvent && d3.event.sourceEvent.type === "zoom") return;
+
+        var s = arrangeSelection(d3.event.selection || [[0,0],[0,0]]);
+
+        if(d3.sum(flatten(s)) > 0) {
+            lastExtent = s;
+            zoomChart([x.invert(s[0][0]),x.invert(s[1][0])], [y.invert(s[0][1]),y.invert(s[1][1])])
+            svg.select('g.brush').call(brush.move, [[0,0],[0,0]])
+        } else if(d3.sum(flatten(lastExtent))==0) {
+                zoomChart(domain.x, domain.y)
+        } else {
+            lastExtent = [[0,0],[0,0]];
+        }
+    }
     
 
   }
